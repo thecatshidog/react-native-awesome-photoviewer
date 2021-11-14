5a4094c05f185b68124a710d4a35f137d4ae815c
shiyanan <979248537@qq.com>
2021-11-14 12:43:52 +0800

    fix android build error v1.0

--- android/src/main/java/com/reactnativeawesomephotoview/glide/MyAppGlideModule.java
+++ android/src/main/java/com/reactnativeawesomephotoview/glide/MyAppGlideModule.java
@@ -9,14 +9,14 @@ import com.bumptech.glide.Registry;
 import com.bumptech.glide.annotation.GlideModule;
 import com.bumptech.glide.integration.okhttp3.OkHttpUrlLoader;
 import com.bumptech.glide.load.model.GlideUrl;
-import com.bumptech.glide.module.AppGlideModule;
+import com.bumptech.glide.module.LibraryGlideModule;
 
 import java.io.InputStream;
 
 import cc.shinichi.library.glide.progress.ProgressManager;
 
 @GlideModule
-public class MyAppGlideModule extends AppGlideModule {
+public class MyAppGlideModule extends LibraryGlideModule {
   @Override
   public void registerComponents(@NonNull Context context, @NonNull Glide glide, @NonNull Registry registry) {
     super.registerComponents(context, glide, registry);
